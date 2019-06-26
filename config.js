9f304cdf476fb106722f5e9806a12b65001c710c
Federico Vitale <fedevitale99@gmail.com>
2019-06-27 00:09:06 +0200

    cleanup

--- config.js
+++ config.js
@@ -5,20 +5,20 @@ module.exports = {
   description: "Federico Vitale's blog",
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by Federico Vitale ...
-  shortBio: '',
+  shortBio: 'Impossible just takes longer.',
   // a longer bio showing on the landing page of the blog
   bio: `Javascript & Swift — Love sharing what I learn day by day.`,
   author: 'Federico Vitale',
   githubUrl: 'https://github.com/Rawnly/blog.fedevitale.dev',
   // replace this by the url where your website will be published
-  siteUrl: 'fedevitale-blog.netlify.com',
+  siteUrl: 'https://blog.devitale.dev',
   social: {
     // leave the social media you do not want to appear as empty strings
-    twitter: '',
+    twitter: '@fedevitaledev',
     medium: '@fede.vitale',
     facebook: '',
-    github: '',
+    github: 'rawnly',
     linkedin: '',
-    instagram: '',
+    instagram: '@fedevitale.dev',
   },
 }
