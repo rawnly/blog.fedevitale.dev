a5509fa3c05aed7fcdcdf045dea0cf4ad784a4e7
Federico Vitale <fedevitale99@gmail.com>
2019-06-26 23:37:36 +0200

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Federico Vitale',
   githubUrl: 'https://github.com/Rawnly/blog.fedevitale.dev',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'fedevitale-blog.netlify.com',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: '',
